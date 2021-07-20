81d6c927132e1d52f5f68295d5cc9cc6c25b40c4
bfinecpa1@naver.com <bfinecpa1@naver.com>
2021-07-20 14:03:33 +0900

    up

--- search.js
+++ search.js
@@ -1,7 +1,6 @@
 //클릭시 주황색 테두리
 
 var file_description = document.querySelectorAll(".file_description");
-
 for(let i = 0; i<file_description.length; i++)
 {
     
@@ -29,9 +28,52 @@ function change_ph(x)
 {
     var value=x.value;
     var ph= document.querySelector(".search_text");
-    ph=ph.getElementsByTagName("input")[0];
-    ph.placeholder="검색할 "+ value + " 입력해주세요";
+    var search_text = document.getElementsByClassName("search_text");
+    if(value=="날짜")
+    {
+        search_text[0].classList.add("display_none");
+        search_text[1].classList.remove("display_none")
+    }
+    else
+    {
+        search_text[0].classList.remove("display_none");
+        search_text[1].classList.add("display_none")
+        ph=ph.getElementsByTagName("input")[0];
+        ph.placeholder="검색할 "+ value + " 입력해주세요";
+    }
+    
+    
 }
 
+// 리스트  형태일시 호버시 클릭시 투명도
+/*
+var list_contents_li = document.getElementsByClassName("list_contents_li")
+for(var i = 0; i <list_contents_li.length; i++)
+{
+    list_contents_li[i].addEventListener("click", ()=>
+    {
+    
+        list_contents_li[i].classList.add("click_background");
+    })
+}*/ 
+// ------------------위에 코드 안됨 이유 찾아야함 ----------------------
+    
 
+//보기 형식에 따른
 
+function change_view(x)
+{
+    var value= x.value;
+    var list_contents = document.querySelector(".list_contents");
+    var thumbnail_contents = document.querySelector(".thumbnail_contents");
+    if(value=="썸네일형 보기")
+        {
+            thumbnail_contents.classList.remove("display_none");
+            list_contents.classList.add("display_none");
+        }
+    else
+    {
+        list_contents.classList.remove("display_none");
+        thumbnail_contents.classList.add("display_none");
+    }
+}
