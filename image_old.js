0232829e1a1f4e6f6511818ad2d2978fd68ae8b3
bfinecpa1@naver.com <bfinecpa1@naver.com>
2021-08-13 10:20:58 +0900

    ok

---
+++ image_old.js
@@ -0,0 +1,226 @@
+
+
+var image_list = null;
+var image_scroll_veil = null;
+image_list = document.getElementsByClassName("image_list");
+image_scroll_veil = document.getElementsByClassName("image_scroll_veil");
+    for (let i = 0; i < image_scroll_veil.length; i++) {
+        image_scroll_veil[i].addEventListener("click", () => {
+        checkRadio();
+        image_list[i].classList.toggle("active");
+        });
+    }
+
+function checkRadio() {
+    for (let i = 0; i < image_scroll_veil.length; i++) {
+        if (image_list[i].className == "image_list active") {
+        image_list[i].classList.remove("active");
+        }
+    }
+}
+
+//탭에 따른 변경
+var image_tab_check = document.querySelector(".tab_imagelist").querySelector("input");
+var tab_box=document.querySelector(".imagetab");
+
+tab_box.addEventListener("click",()=>
+{
+    image_tab();
+})
+function image_tab()
+{
+    if(image_tab_check.checked)
+    {
+        document.querySelector("#option_sort").style.display="block";
+        document.querySelector("#option_vote").style.display="none";
+        document.querySelector(".image_send_btn").style.display="block";
+        document.querySelector(".image_all_delete_btn").style.display="block";
+        document.querySelector(".vote_before").style.display="none";
+        document.querySelector(".duplicate_vote_off").style.display="none";
+    }
+
+    else
+    {
+        document.querySelector("#option_sort").style.display="none";
+        document.querySelector("#option_vote").style.display="block";
+        document.querySelector(".image_send_btn").style.display="none";
+        document.querySelector(".image_all_delete_btn").style.display="none";
+        document.querySelector(".vote_before").style.display="block";
+        document.querySelector(".duplicate_vote_off").style.display="block";
+    }
+}
+
+//투표시작 중복투표 버튼 변경
+var vote_before = document.querySelector(".vote_before");
+vote_before.addEventListener("click",()=>
+{
+    if(!vote_before.classList.contains("vote_before_active"))
+    {
+        vote_before.classList.add("vote_before_active");
+        for (let i = 0; i < image_scroll_veil.length; i++) {
+            image_list[i].querySelector(".image_voting_number").style.display="block";
+        }
+    }   
+    else
+    {
+        vote_before.classList.remove("vote_before_active");
+        for (let i = 0; i < image_scroll_veil.length; i++) {
+            image_list[i].querySelector(".image_voting_number").style.display="none";
+        }
+    }
+        
+})
+
+var duplicate_vote_off=document.querySelector(".duplicate_vote_off");
+duplicate_vote_off.addEventListener("click", ()=> {
+    if(!duplicate_vote_off.classList.contains("duplicate_vote_off_active"))
+        duplicate_vote_off.classList.add("duplicate_vote_off_active");
+    else
+        duplicate_vote_off.classList.remove("duplicate_vote_off_active");
+})
+
+// 하나 삭제
+var image_delete_btn = document.querySelectorAll(".image_delete_btn");
+
+for (let i = 0; i < image_scroll_veil.length; i++) {
+    image_delete_btn[i].addEventListener("click", ()=>{
+        document.querySelector(".image_popbackground").style.display="block";
+        document.querySelector(".image_popdelete_box").style.display="block";
+    }
+)
+}
+
+var modal_button_positive=document.querySelector(".modal_button_positive");
+modal_button_positive.addEventListener("click", ()=>{
+    document.querySelector(".image_popbackground").style.display="none";
+    document.querySelector(".image_popdelete_box").style.display="none";
+})
+
+var modal_button_negative=document.querySelectorAll(".modal_button_negative");
+modal_button_negative[1].addEventListener("click", ()=>{
+    document.querySelector(".image_popbackground").style.display="none";
+    document.querySelector(".image_popdelete_box").style.display="none";
+})
+
+//모두삭제
+var image_all_delete_btn = document.querySelector(".image_all_delete_btn");
+image_all_delete_btn.addEventListener("click", ()=>{
+    document.querySelector(".image_popbackground").style.display="block";
+    document.querySelector(".image_popdelete_all_box").style.display="block";
+})
+
+var modal_button_all_delete = document.querySelector(".modal_button_all_delete");
+modal_button_all_delete.addEventListener("click",()=>{
+    document.querySelector(".image_popbackground").style.display="none";
+    document.querySelector(".image_popdelete_all_box").style.display="none";
+})
+modal_button_negative[0].addEventListener("click", ()=>{
+    document.querySelector(".image_popbackground").style.display="none";
+    document.querySelector(".image_popdelete_all_box").style.display="none";
+})
+
+// 사진 및 동영상 보내기 팝업창 뛰우기
+
+var image_send_btn= document.querySelector(".image_send_btn");
+image_send_btn.addEventListener("click", ()=>{
+    document.querySelector(".image_send_popbox").style.display="block";
+    document.querySelector(".image_popbackground").style.display="block";
+})
+
+var image_send_poptop_expel= document.querySelector(".image_send_poptop_expel");
+image_send_poptop_expel.addEventListener("click",()=>{
+    document.querySelector(".image_send_popbox").style.display="none";
+    document.querySelector(".image_popbackground").style.display="none";
+})
+
+
+//이미지 업로드
+
+function load_image(input)
+{
+
+    var image_send_pop_uplodebox_background = document.querySelector(".image_send_pop_uplodebox_background");
+    var file = input.files[0];
+    var newimage = document.createElement("img");
+    newimage.setAttribute("class", "load_newimage");
+    newimage.src=URL.createObjectURL(file);
+    
+    image_send_pop_uplodebox_background.getElementsByTagName("label")[0].style.display="none";
+
+    var delete_imgtag = image_send_pop_uplodebox_background.getElementsByTagName("img")[0];
+    var delete_divtag = image_send_pop_uplodebox_background.getElementsByTagName("div")[7];
+    if(image_send_pop_uplodebox_background.getElementsByClassName("load_newimage")[0])
+    {
+        image_send_pop_uplodebox_background.removeChild(delete_imgtag);
+        image_send_pop_uplodebox_background.removeChild(delete_divtag);
+    }
+    image_send_pop_uplodebox_background.appendChild(newimage);
+    newimage.style.width="311px";
+    newimage.style.height="175px";
+    newimage.style.position="absolute";
+    newimage.style.top="59px";
+    edit_image();
+}
+
+function edit_image() {
+    const image = document.getElementsByClassName("load_newimage")[0];
+    cropper = new Cropper(image, {
+    viewMode: 0,
+    dragMode: "move",
+    aspectRatio: 16 / 9,
+    center: false,
+    highlight: false,
+    background: false,
+    autoCropArea: 1,
+    });
+}
+
+//그리드
+var image_send_pop_controllbox_gridon = document.querySelector(".image_send_pop_controllbox_gridon");
+image_send_pop_controllbox_gridon.addEventListener("click", ()=>
+{
+    image_send_pop_controllbox_gridon.classList.toggle("image_send_pop_controllbox_gridoff")
+    if(image_send_pop_controllbox_gridon.classList.contains("image_send_pop_controllbox_gridoff"))
+    {
+        document.querySelector(".cropper-container").style.display="none";
+        document.querySelector(".load_newimage").style.display="block";
+        document.querySelector(".load_newimage").classList.remove("cropper-hidden");
+        document.querySelector(".image_edge_point").style.display="block";
+    }
+    else
+    {
+        document.querySelector(".cropper-container").style.display="block";
+        document.querySelector(".load_newimage").style.display="none";
+        document.querySelector(".load_newimage").classList.add("cropper-hidden");
+        document.querySelector(".image_edge_point").style.display="none";
+    }
+})
+
+//회전
+var image_send_pop_controllbox_rotate_left = document.querySelector(".image_send_pop_controllbox_rotate_left");
+image_send_pop_controllbox_rotate_left.addEventListener("click", () => {
+    cropper.rotate(-90);
+})
+
+var image_send_pop_controllbox_rotate_right = document.querySelector(".image_send_pop_controllbox_rotate_right");
+image_send_pop_controllbox_rotate_right.addEventListener("click", () => {
+    cropper.rotate(90);
+})
+
+var image_send_pop_controllbox_confirm = document.querySelector(".image_send_pop_controllbox_confirm");
+image_send_pop_controllbox_confirm.addEventListener("click",()=>{
+    get_image();
+})
+
+function get_image() {
+    var image_send_pop_uplodebox_background = document.querySelector(".image_send_pop_uplodebox_background");
+        newCanvas = document.createElement("canvas");
+        newCanvas.setAttribute("id", "newCanvas");
+        newCanvas = cropper.getCroppedCanvas({
+        width: 311,
+        height: 175,
+        });
+        image_send_pop_uplodebox_background.appendChild(newCanvas);
+        document.querySelector(".cropper-crop-box").classList.toggle("d_none")
+        
+    }
---
+++ search2_old.js
@@ -0,0 +1,373 @@
+//data object
+let list_data = [
+/*
+  {
+    list_img:
+      "https://docs.google.com/presentation/d/1wJeY0T4ZGlCSyo2YnKiNRSvai84FDd-CdaC9FUJrU-c/preview?rm=minimal&slide=id.SLIDES_API1655905382_0",
+    list_file_name: "coordinator_20210702",
+    list_profile_img: "./search_img/list_profile.png",
+    list_profile_name: "홍길동",
+    list_date: "2021년 7월 2일",
+    list_desc: "코로나 없는 추억여행",
+    total_img: 34,
+  },
+  {
+    list_img:
+      "https://docs.google.com/presentation/d/1wJeY0T4ZGlCSyo2YnKiNRSvai84FDd-CdaC9FUJrU-c/preview?rm=minimal&slide=id.SLIDES_API1655905382_0",
+    list_file_name: "coordinator_20210702",
+    list_profile_img: "./search_img/list_profile.png",
+    list_profile_name: "홍길동",
+    list_date: "2021년 7월 2일",
+    list_desc: "코로나 없는 추억여행",
+    total_img: 34,
+  },
+  {
+    list_img:
+      "https://docs.google.com/presentation/d/1wJeY0T4ZGlCSyo2YnKiNRSvai84FDd-CdaC9FUJrU-c/preview?rm=minimal&slide=id.SLIDES_API1655905382_0",
+    list_file_name: "coordinator_20210702",
+    list_profile_img: "./search_img/list_profile.png",
+    list_profile_name: "홍길동",
+    list_date: "2021년 7월 2일",
+    list_desc: "코로나 없는 추억여행",
+    total_img: 34,
+  },
+  {
+    list_img:
+      "https://docs.google.com/presentation/d/1wJeY0T4ZGlCSyo2YnKiNRSvai84FDd-CdaC9FUJrU-c/preview?rm=minimal&slide=id.SLIDES_API1655905382_0",
+    list_file_name: "coordinator_20210702",
+    list_profile_img: "./search_img/list_profile.png",
+    list_profile_name: "홍길동",
+    list_date: "2021년 7월 2일",
+    list_desc: "코로나 없는 추억여행",
+    total_img: 34,
+  },
+  {
+    list_img:
+      "https://docs.google.com/presentation/d/1wJeY0T4ZGlCSyo2YnKiNRSvai84FDd-CdaC9FUJrU-c/preview?rm=minimal&slide=id.SLIDES_API1655905382_0",
+    list_file_name: "coordinator_20210702",
+    list_profile_img: "./search_img/list_profile.png",
+    list_profile_name: "홍길동",
+    list_date: "2021년 7월 2일",
+    list_desc: "코로나 없는 추억여행",
+    total_img: 34,
+  },
+  */
+  /*
+  {
+    list_img: "./search_img/img100.jpg",
+    list_file_name: "coordinator_20200216",
+    list_profile_img: "./search_img/list_profile.png",
+    list_profile_name: "김하나",
+    list_date: "2020년 2월 16일",
+    list_desc: "2020년 겨울 친구들과 함께한 여행",
+    total_img: 24,
+  },
+  {
+    list_img: "./search_img/img100.jpg",
+    list_file_name: "coordinator_20191230",
+    list_profile_img: "./search_img/list_profile.png",
+    list_profile_name: "박명수",
+    list_date: "2019년 12월 30일",
+    list_desc: "작년겨울 가족끼리 모여서 찍은 사진",
+    total_img: 18,
+  },
+  {
+    list_img: "./search_img/img100.jpg",
+    list_file_name: "coordinator_2021327",
+    list_profile_img: "./search_img/list_profile.png",
+    list_profile_name: "안산",
+    list_date: "2021년 3월 27일",
+    list_desc: "생일날 모여서 찍은 사진",
+    total_img: 15,
+  },*/
+];
+add_whole_data();
+
+function add_whole_data() {
+  if(document.title=="search"){
+    //목록형보기 data추가
+    add_list_data();
+    //썸네일형보기 data추가
+    add_thumnails_data();
+  }
+  else if(document.title=="search_mob"){
+    add_mob_list_data();
+    add_mob_thumnails_data();
+  }
+  
+}
+
+//목록형 보기 data를 list_contents_ul에 추가
+function add_list_data() {
+  const ul = document.getElementsByClassName("list_contents_ul")[0];
+  let html = "";
+  //ul안에 넣을 html 합치기
+  list_data.forEach((data, index) => {
+    index += 1; //넘버링 1부터 시작
+    html += `<div class="list_contents_li">
+        <div class="list_file_num">${index < 10 ? "0" + index : index}</div>
+        <div class="list_iframe">
+          <iframe class="list_file_image" src="${data.list_img}" ></iframe>
+          <div class="list_veil"></div>
+        </div>
+        <div class="list_file_images_num">외 ${data.total_img - 1}장</div>
+        <div class="list_file_name">${data.list_file_name}</div>
+        <div class="list_file_coordn">
+            <img src="${data.list_profile_img}" alt="">
+            <div style="width: 8px;"></div>
+            ${data.list_profile_name}
+        </div>
+        <div class="list_file_mdate">${data.list_date}</div>
+        <div class="list_file_desc">${data.list_desc}</div>
+        <div class="view_down">
+            <button class="view_file" onclick="open_presentation('${data.list_img_org}')">
+                <img src="./search_img/view_alt_white_24dp.png" alt="">
+                <span>파일보기</span>
+            </button>
+            <div style="height: 8px;"></div>
+            <button class="down_file">
+                <img src="./search_img/save_alt_white_24dp.png" alt="">
+                <span>다운로드</span>
+            </button>
+        </div>
+    </div>`;
+  });
+  //더한 html을 ul안에 넣기
+  ul.innerHTML = html;
+}
+
+//썸네일형 보기 data를 thumbnail_contents에 추가
+function add_thumnails_data() {
+  const ul = document.getElementsByClassName("thumbnail_contents")[0];
+  let html = "";
+  //row안에 넣을 html 합치기
+  list_data.forEach((data, index) => {
+    html += `<div class="file_wrap">
+        <div class="file_num">${index + 1 < 10 ? "0" + (index + 1) : index + 1}</div>
+        <div class="file_description">
+          <iframe class="file_thumbnail" src="${data.list_img}" ></iframe>
+            <div class="file_image_num">외 ${data.total_img - 1}장</div>
+            <div class="file_data">
+                <div class="user_icon">
+                    <img src="${data.list_profile_img}" alt="">
+                </div>
+                <div>
+                    <div class="file_name">${data.list_file_name}</div>
+                    <div class="file_user_date flex-row">
+                        <div class="file_user">${data.list_profile_name}</div>
+                        <div>&nbsp;&nbsp;|&nbsp;&nbsp;</div>
+                        <div class="file_date">${data.list_date}</div>
+                    </div>
+                </div>
+            </div>
+            <div class="file_message">${data.list_desc}</div>
+            <div class="file_veil"></div>
+            <div class="btn_wrap">
+            <button>
+                <img src="./search_mob_img/save_alt_white_24dp@2x.png" width="26px" height="26px" alt="">
+                <div style="width: 8px;"></div>
+                파일보기
+            </button>
+            <div style="width: 9px;"></div>
+            <button>
+                <img src="./search_mob_img/down_alt_white_24dp@2x.png" width="26px" height="26px" alt="">
+                <div style="width: 8px;"></div>
+                다운로드
+            </button>
+        </div>
+        </div>
+    </div>`;
+    //3개 단위로 row안에 넣기
+    if (index % 3 == 2) {
+      let div = document.createElement("div");
+      div.classList.add("contents_row");
+      div.innerHTML += html;
+      ul.appendChild(div);
+      html = "";
+    }
+  });
+  //남은 data 추가
+  let div = document.createElement("div");
+  div.classList.add("contents_row");
+  div.innerHTML += html;
+  ul.appendChild(div);
+}
+function open_presentation(url)
+{
+  document.querySelector("#org_present").src = url;
+  document.querySelector("#tobe_full").classList.remove("d-none");
+  document.querySelector("#main_div").classList.add("d-none");
+
+  
+}
+function close_presentation()
+{
+  document.querySelector("#org_present").src = "";
+  document.querySelector("#tobe_full").classList.add("d-none");
+  document.querySelector("#main_div").classList.remove("d-none");
+ 
+}
+
+function search_specific_data() {
+  if (!parent.ready_history_data) {
+    alert("not ready");
+    return;
+  }
+
+  document.querySelector(".thumbnail_contents").innerHTML = "";
+  var svalue = document.getElementById("search_input").value;
+  parent.sunny.find_from_history(svalue,current_search_mode, function (rst) {
+    console.log(rst);
+    list_data = [];
+    var search_sheet = parent.sunny.get_current_search_sheet();
+    for (var i = 0; i < rst.length; i++) {
+      var each_list_data = {
+        list_img: "",
+        list_img_org:"",
+        list_file_name: "coordinator_20200216",
+        list_profile_img: "./search_img/list_profile.png",
+        list_profile_name: "김하나",
+        list_date: "2020년 2월 16일",
+        list_desc: "",
+        total_img: 24,
+      };
+      var url =
+        "https://docs.google.com/presentation/d/" +
+        rst[i].PresentationID +
+        "/preview?slide=id." +
+        rst[i].SlideId +
+        "&rm=minimal";
+
+      each_list_data.list_img = url;
+//https://docs.google.com/presentation/d/12sHoVwWWxdEVVwlUCnZ5Sp5_fKwe4vsAU1_tpQRMKpI/edit#slide=id.SLIDES_API62210478_0
+      url =
+        "https://docs.google.com/presentation/d/" +
+        rst[i].OriginPresent +
+        "/edit#slide=id." +
+        rst[i].SlideId_Org
+        
+
+      each_list_data.list_img_org = url;
+      
+
+      if(search_sheet =="Presentations")
+      {
+        each_list_data.list_profile_name = rst[i].CoordiName;
+        each_list_data.list_desc = rst[i].ConferenceExplain;
+        each_list_data.list_file_name = rst[i].ConferenceName;
+        
+      }
+      else
+      {
+        each_list_data.list_profile_name ="";
+        each_list_data.list_desc = rst[i].Comment;
+      }
+        
+      list_data.push(each_list_data);
+
+      console.log(url);
+    }
+    add_whole_data();
+  });
+  document.getElementById("search_input").value = "";
+}
+
+
+
+
+
+
+
+
+
+//-----------------------------------------------모바일 일때 가져온 것--------------------------------------------
+function add_mob_list_data() {
+  const ul = document.getElementsByClassName("list_contents_ul")[0];
+  let html = "";
+  //ul안에 넣을 html 합치기
+  list_data.forEach((data, index) => {
+    index += 1; //넘버링 1부터 시작
+    html += `<div class="list_contents_li">
+    <div class="list_file_num">${index < 10 ? "0" + index : index}</div>
+    <div class="list_file_report">
+        <div class="list_img_imfo_wrap">
+            <div class="list_iframe">
+              <iframe class="list_file_image" src="${data.list_img}" ></iframe>
+              <div class="list_veil"></div>
+            </div>
+            <div class="restofimagenum">외 ${data.total_img - 1}장</div>
+            <div class="list_file_name">${data.list_file_name}</div>
+            <div class="list_file_coordn">
+                <img src="${data.list_profile_img}" alt="">
+                <div  style="width: 8px;"></div>
+                ${data.list_profile_name}
+                <div style="margin-left: 5px;">|</div>
+            </div>
+            <div class="list_file_mdate">${data.list_date}</div>
+        </div>
+        <div class="list_file_desc">${data.list_desc}</div>
+        <div class="btn_wrap">
+            <button >
+                <img src="./search_mob_img/save_alt_white_24dp@2x.png" width="26px" height="26px" alt="">
+                <div style="width: 8px;"></div>
+                파일보기
+            </button>
+            <div style="width: 9px;"></div>
+            <button>
+                <img src="./search_mob_img/down_alt_white_24dp@2x.png" width="26px" height="26px" alt="">
+                <div style="width: 8px;"></div>
+                다운로드
+            </button>
+        </div>
+    </div>
+</div>`;
+  });
+  //더한 html을 ul안에 넣기
+  ul.innerHTML = html;
+}
+
+function add_mob_thumnails_data() {
+  const ul = document.getElementsByClassName("thumbnail_contents")[0];
+  let html = "";
+  //row안에 넣을 html 합치기
+  list_data.forEach((data, index) => {
+    html += `<div class="file_wrap">
+    <div class="file_num">${index + 1 < 10 ? "0" + (index + 1) : index + 1}</div>
+    <div class="file_description ">
+        <iframe class="file_thumbnail" src="${data.list_img}" ></iframe>
+        <div  class="file_thumbnail_veil"></div>
+        <div class="file_image_num">외 ${data.total_img - 1}장</div>
+        <div class="file_data">
+            <div class="user_icon">
+                <img src="${data.list_profile_img}" alt="">
+            </div>
+            <div>
+                <div class="file_name"> ${data.list_file_name}</div>
+                <div class="file_user_date flex-row">
+                    <div class="file_user">${data.list_profile_name}</div>
+                    <div>&nbsp;&nbsp;|&nbsp;&nbsp;</div>
+                    <div class="file_date">${data.list_date}</div>
+                </div>
+            </div>
+            
+        </div>
+        <div class="file_message">${data.list_desc}</div>
+        <div class="btn_wrap">
+            <button>
+                <img src="./search_mob_img/save_alt_white_24dp@2x.png" width="26px" height="26px" alt="">
+                <div style="width: 8px;"></div>
+                파일보기
+            </button>
+            <div style="width: 9px;"></div>
+            <button>
+                <img src="./search_mob_img/down_alt_white_24dp@2x.png" width="26px" height="26px" alt="">
+                <div style="width: 8px;"></div>
+                다운로드
+            </button>
+        </div>
+    </div>
+</div>`;
+  });
+  ul.innerHTML = html;
+}
+
